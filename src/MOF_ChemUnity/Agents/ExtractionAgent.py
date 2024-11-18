5ce468fbc738ae3825fb0559ed2e732d34b69169
Amro Aswad <amro.aswad2a@gmail.com>
2024-11-18 14:03:33 -0500

    added the Extraction Agent file. I hope it works, it is still missing some documentation.

---
+++ src/MOF_ChemUnity/Agents/ExtractionAgent.py
@@ -0,0 +1,236 @@
+from genericpath import isfile
+from os import path
+from typing import Dict, List, Optional
+from pydantic import BaseModel, Field
+from regex import W
+from thefuzz import fuzz
+
+from langchain_openai import ChatOpenAI
+from langchain.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores import FAISS
+
+import BaseAgent
+from utils.DocProcessor import DocProcessor
+from utils.DataModels import Property, Verification, PropertyList
+
+PROPERTIES = ["chemical formula", "density", "crystal system", "cell volume", "space group", "molecular weight", "material color", 
+         "decomposition temperature", "magnetic susceptibility", "surface area", "pore volume", "pore diameter", "porosity", "topology",
+         "magnetic moment", "melting point", "proton conductivity", "elastic constant", "thermal expansion coefficient", "heat capacity",
+         "thermal conductivity coefficient"]
+
+class ExtractionAgent(BaseAgent):
+    def __init__(
+            self,
+            llm=ChatOpenAI(model="gpt-4", temperature=0.1),
+            embeddings=OpenAIEmbeddings(model="text-embedding-ada-002"),
+            parser_llm=None,
+            structured_llm: bool = True,
+            processor: Optional[DocProcessor] = None,
+            properties: Optional[List[str]] = None):
+
+        super().__init__(llm, embeddings, parser_llm, structured_llm, processor)
+
+        if not properties:
+            self.properties = PROPERTIES
+        else:
+            self.properties = properties
+            
+        pass
+
+
+    def filter_properties(
+            self,
+            extracted_props: List[Property],
+            props_filter: Optional[List[str]] = None,
+            threshold: int = 90):
+        """This function filters the extracted properties list based on the props_filter list given.
+        It goes over all combinations of extracted_properties and props_filter, then the value with the maximum
+        match score that is higher than the threshold is added to the filtered list.
+        
+        returns a tuple of length 2 containing the following:
+            -filtered properties: the properties that were matched with the filter
+            -remaining properties: the properties that did not get matched """
+
+        # Loop setup
+        i = 0
+        filtered_props = []
+
+        # copies the values to prevent list.pop from editing the list
+        props = [i for i in (props_filter if props_filter else self.properties)]
+
+        # loop through all the combinations
+        while(i<len(props)):
+            max_score = -1
+            max_index = -1
+            for j, v in enumerate(extracted_props):
+                # This matching strategy compares the token sets to each other (Order doesn't matter).
+                # It needs to be changed if there are properties that have the same token set but different order.
+                score = fuzz.token_set_ratio(props[i].lower(), v.name.lower())
+                if score>max_score and score>=threshold:
+                    max_score = score
+                    max_index = j
+
+            if max_score >= 0:
+                found_property = props.pop(max_index)
+                found_property.name = props[i]
+                filtered_props.append(found_property)
+            
+            i+=1
+        
+        return (filtered_props, props)
+    
+    def extraction_CoV(
+            self,
+            MOF: str,
+            vector_store,
+            specific_properties: List[str],
+            read_prompts = List[str],
+            verification_prompts = List[str],
+            recheck_prompts = List[str],
+            ret_docs: bool = False):
+
+        """This is the extraction with Chain of Verification (CoV) function. It takes a more vigorous path to find the correct output.
+        
+        Use must specify the list of specific properties they are looking for. They must then also provide the following:
+            -Property-specific read-prompts list
+            -List of verification prompts for every specific property
+            -List of recheck prompts for every specific property
+
+        Note: the properties filter is not used in this function!
+        
+        The return is a tuple of length 2 containing the following:
+            -List of properties with the same length as specific_properties. This is not a PropertyList object, it is simply a list of Property objects!
+            -All of the text that the LLM looked through to find the output. This is None if ret_docs is False
+        """
+        
+        if len(specific_properties) != len(read_prompts) or \
+            len(specific_properties) != len(verification_prompts) or \
+            len(specific_properties) != len(recheck_prompts):
+            print("The lengths of read_prompts, verification_prompts and recheck_prompts must be the same as the number of properties given in specific_properties.")
+            raise AssertionError()
+
+        specific_property_return = []
+        parser = self.Parse_Output(Property)
+        verification_parser = self.Parse_Output(Verification)
+        
+        for i, specific_property in enumerate(specific_properties):
+
+            print(f"Reading to find the {specific_property} of {MOF} specifically")
+            (answer1, docs) = self.RAG_Chain_Output(read_prompts[i].format(MOF_name=MOF), vector_store)
+        
+            print("LLM Output: ")
+            print(answer1["result"])
+
+            property = parser.invoke(f"Parse the following text into the structured output.\Text:\n{answer1['result']}")
+
+            print("\nParsed Output:")
+            print(property)
+        
+            if (property.value.lower() == "not provided"):
+                print(f"\nCould not find the {specific_property} in the document")
+                specific_property_return.append(property)
+                continue
+
+            print("\nVerifying the extraction:")
+
+            answer2 = self.llm.invoke(verification_prompts[i].format(output=property, Property_name=specific_property, MOF_name=MOF))
+            verification = verification_parser.invoke(f"Parse the following text into the structured output.\nText:\n{answer2.content}")
+
+            if(verification.valid):
+                specific_property_return.append(property)
+                continue
+
+            print("\nReading the document again to find a different justification/label")
+
+            (answer3, docs) = self.RAG_Chain_Output(recheck_prompts[i].format(MOF_name=MOF, output=property, Property_name=specific_property), vector_store)
+
+            print("LLM Output from Rechecking: ")
+            print(answer3["result"])
+
+
+            property = parser.invoke(f"Parse the following text into the structured output.\Text:\n{answer3['result']}")
+            print("\nParsed Output:")
+            print(property)
+
+            specific_property_return.append(property)
+
+        if ret_docs:
+            return specific_property_return, docs
+        else:
+            return specific_properties, None
+    
+
+    def property_extraction(
+            self,
+            MOF: str,
+            read_prompt: str,
+            vector_store,
+            ret_docs: bool = False,
+            filtered: bool = True,
+            filter: Optional[List[str]] = None,
+            threshold: int = 90):
+        
+        parser = self.Parse_Output(PropertyList)
+
+        print("Action: reading the document")
+        names = " ---".join([f"name {i+1}: {name.replace('{', ']').replace('}', ']')}" for i, name in enumerate(MOF.split("<|>"))])
+        print(f"finding all properties of {names}")
+
+        (answer1, docs) = self.RAG_Chain_Output(read_prompt.format(MOF_name=names), vector_store)
+
+        print("\nResult: ")
+        print(answer1["result"])
+
+        list_properties = parser.invoke(f"Parse the following text into the structured output\nText:\n{answer1['result']}")
+
+        print("\nParsed Result: ")
+        print(list_properties)
+        print("\n")
+
+        if not filtered: return (list_properties, docs) if ret_docs else list_properties
+
+        filtered, remaining = self.filter_properties(list_properties, filter, threshold)
+
+        print("\nFiltered Output: ")
+        for i in filtered:
+            print(i)
+        print("\n")
+
+        return (filtered, list_properties, docs) if ret_docs else (filtered, list_properties)
+
+
+    def agent_response(
+            self,
+            MOF: str,
+            paper_file_name: str,
+            read_prompt: str,
+            specific_properties: List[str] = None,
+            specific_properties_prompts: Dict[str, List[str]] = None,
+            vector_store: Optional[str|FAISS] = None,
+            local_vector_store_embeddings: Optional[str] = None,
+            filter: Optional[List[str]] = None,
+            ret_docs: bool = False,
+            filtered: bool = True,
+            store_vs: bool = False,
+            CoV: bool = False,
+            skip_general: bool = False,
+            fuzz_threshold: int = 90):
+        
+        if not vector_store:
+            vector_store = self.create_vector_store(paper_file_name, store_vs=store_vs)
+        elif type(vector_store) is str:
+            if path.isfile(vector_store):
+                vector_store = FAISS.load_local(vector_store, local_vector_store_embeddings if local_vector_store_embeddings else self.embeddings)
+            else:
+                vector_store = self.create_vector_store(paper_file_name, store_vs=store_vs, store_folder=vector_store)
+
+        general_response = None
+
+        if not skip_general: 
+            general_response = self.property_extraction(MOF, read_prompt, vector_store, ret_docs, filtered, filter, fuzz_threshold)
+
+        if CoV:
+            specific_resposne = self.extraction_CoV(MOF, vector_store, specific_properties, **specific_properties_prompts, ret_docs)
+            return general_response, specific_resposne
+        
+        return general_response
