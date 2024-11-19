4ecf414fbb9a45ec6507622732430eebf0fe9f99
Amro Aswad <amro.aswad2a@gmail.com>
2024-11-19 14:16:53 -0500

    added separate prompt files for water stability extraction and general property extraction

---
+++ src/MOF_ChemUnity/Extraction_Prompts.py
@@ -0,0 +1,27 @@
+EXTRACTION = """You are an expert in coordinated chemistry and Metal Organic Frameworks (MOF).
+You need to extract relevant physical, chemical and structural properties for a MOF that has the following names: {MOF_name}.
+Please be precise and try to be as accurate as possible. Make sure all properties are related ONLY to the MOF Names provided.
+
+Your output should have the following format for each property and its value:
+1.  -Property Name: the name of the extracted property. Be consistent and use field relevant names. Do not include units here.
+    -Property Value: the value for the property, please be precise and do not include units here, you need to fix any formatting issues.
+    -Value Units: the units for this property. Include percentages, letters and signs that refer to units. You need to fix any formatting issues.
+    -Conditions: the experimental conditions that this value was observed at.
+    -Summary: The exact sentences from the provided text that mention the MOF Name and this property."""
+
+VERIFICATION = """Do the below justification sentences actually talk about the {Property_name} of the {MOF_name}?
+
+Extracted Output: {output}
+
+{VERF_RULES}
+
+Start your answer clearly with "Valid" or "Invalid".
+"""
+
+RECHECK = """You are an expert chemist. In the document find the {Property_name} of the MOF with the following Names and Coreferences: {MOF_name}.
+Previously, you suggested the following extraction which was INCORRECT:
+
+Previous Extraction: {output}
+
+{RECHECK_INSTRUCTIONS}
+"""
---
+++ src/MOF_ChemUnity/Water_Stability_Prompts.py
@@ -0,0 +1,41 @@
+RULES_WATER_STABILITY = """
+There are only 3 options for water stability:
+1. Stable:  -No change in properties after exposure to moisture or steam, soaking or boiling in water or an aqueous solution.
+            -Retaining its porous structure in solution. 
+            -No loss of crystallinity.
+            -Insoluble in water or an aqueous solution.
+            -Water adsorption isotherm should have a steep uptake.
+            -Good cycling performance.
+
+2. Unstable:
+            -The MOF will decompose or change properties or has a change in its crystal structure after exposure/soak to a humid environment, steam or if it partially dissolves in water.
+            -Soluble or patrially soluble in water or an aqueous solution.
+
+
+3. Not provided: If you don't know or cannot justfy 1 or 2
+"""
+
+VERF_RULES_WATER_STABILITY = """To do this, you should check on steep uptakes, solubility in water, change in properties after being exposed to water/steam, change in crystallinity, or mention of water stability in the sentence.
+If the justification can somehow imply water stability/instability, update "Water stability" to Stable/Unstable
+Do not make up answers.
+Do not consider chemical or thermal stability or stability in air as a valid reason."""
+
+WATER_STABILITY = """You are an expert chemist. In the document find the Water Stability of the MOF with the following Names and Coreferences: {MOF_name}. 
+Use the following rules to determine its water stability:
+{RULES}
+
+Your final answer should contain the following:
+    1. The water stability of the MOF. 
+    2. Testing conditions for water stability.
+    3. The exact sentences without any changes from the document that justifies your decision. Try to find more than once sentence. This should be "Not provided" if you cannot find water stability.
+"""
+
+WATER_STABILITY_RE = """This time, you must find a different justification and/or label for the water stability. Or say "Not provided" as outline by the rules below.
+Use the following rules to determine its water stability:
+{RULES}
+
+Your final answer should contain the following:
+    1. The water stability of the MOF. 
+    2. Testing conditions for water stability.
+    3. The exact sentences without any changes from the document that justifies your decision. Try to find more than once sentence. This should be "Not provided" if you cannot find water stability.
+"""
